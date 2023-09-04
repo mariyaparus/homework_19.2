3aa9401aa0cf65059affad9f97ff14c5ba150f90
MariyaParus <cat-she@mail.ru>
2023-09-04 18:44:13 +0300

    upgrade catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='product/', **NULLABLE, verbose_name='Изображение (превью)')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='цена')
+    date_created = models.DateField(**NULLABLE, verbose_name='Дата создания')
+    date_updated = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.category} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукт'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import home, contacts
@@ -7,4 +5,4 @@ from catalog.views import home, contacts
 urlpatterns = [
                   path('', home),
                   path('contacts/', contacts)
-              ] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ]
