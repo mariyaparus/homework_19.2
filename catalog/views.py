37c1902cb691c69036688de75207770cc25bb078
MariyaParus <cat-she@mail.ru>
2023-09-08 23:00:44 +0300

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list[:4],
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def products(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Продукты'
+    }
+    return render(request, 'catalog/products.html', context)
 
 
 def contacts(request):
@@ -13,4 +29,8 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'Имя: {name}\nТелефон: {phone}\nСообщение: {message}')
 
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'catalog/contacts.html', context)
