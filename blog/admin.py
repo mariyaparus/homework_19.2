c8be495dd7591be90ff1965e6dc25deb487175d5
MariyaParus <cat-she@mail.ru>
2023-09-22 15:37:02 +0300

    update admin.py to blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'text',)
+    list_filter = ('title', 'created_at',)
