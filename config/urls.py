1f65242d40ed090f0b51aaa73fafa0081e64adb4
MariyaParus <cat-she@mail.ru>
2023-10-03 11:05:36 +0300

    update config urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('order/', include('order.urls', namespace='order')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
