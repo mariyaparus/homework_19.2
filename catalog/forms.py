25d88494200ede6e6546a2b919daeaf5e4a5cfaa
MariyaParus <cat-she@mail.ru>
2023-10-10 17:30:38 +0300

    update3 forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,10 +4,29 @@ from catalog.models import Product, Version
 
 
 class StyleFormMixin:
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     for field_name, field in self.fields.items():
+    #         field.widget.attrs['class'] = 'form-control'
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+
+            if isinstance(field.widget, forms.widgets.CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+            elif isinstance(field.widget, forms.DateTimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-basic'
+            elif isinstance(field.widget, forms.DateInput):
+                field.widget.attrs['class'] = 'form-control datepicker'
+            elif isinstance(field.widget, forms.TimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-time'
+            elif isinstance(field.widget, forms.widgets.SelectMultiple):
+                field.widget.attrs['class'] = 'form-control select2 select2-multiple'
+            elif isinstance(field.widget, forms.widgets.Select):
+                field.widget.attrs['class'] = 'form-control select2'
+            else:
+                field.widget.attrs['class'] = 'form-control'
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
@@ -19,7 +38,7 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         cleaned_data = self.cleaned_data['name']
         word_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
         for word in word_list:
-            if word.lower() in cleaned_data:
+            if word in cleaned_data.lower():
                 raise forms.ValidationError(f'Использование слова "{word}" в названии запрещено!')
         return cleaned_data
 
@@ -27,9 +46,20 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         cleaned_data = self.cleaned_data['description']
         word_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
         for word in word_list:
-            if word.lower() in cleaned_data:
+            if word in cleaned_data.lower():
                 raise forms.ValidationError(f'Использование слова "{word}" в описании продукта запрещено!')
         return cleaned_data
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     word_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+    #
+    #     for key in cleaned_data.keys():
+    #         for word in word_list:
+    #             if word in cleaned_data[key].lower():
+    #                 raise forms.ValidationError(f'Использование слова "{word}" в {key} запрещено!')
+    #
+    #     return cleaned_data
 
 
 class VersionForm(StyleFormMixin, forms.ModelForm):
@@ -44,3 +74,11 @@ class VersionForm(StyleFormMixin, forms.ModelForm):
     #             f'У вас уже есть активная версия этого продукта.'
     #             f'Если вы хотите сделать активной эту версию, снимите флажок с предыдущей.')
     #     return cleaned_data
+
+    # def clean(self):
+    #     super().clean()
+    #     product_versions_list = []
+    #     for form in Product.objects.get(pk=self.cleaned_data['product'].pk).formset:
+    #         product_versions_list.append(form.cleaned_data['is_active'])
+    #         if product_versions_list.count(True) > 1:
+    #             raise forms.ValidationError('Уже есть активная версия')
